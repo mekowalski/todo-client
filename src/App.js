11685101e20e8111219432efafd851fb9c0c5e37
Malind Kowalski <malind.nikulski@gmail.com>
2018-03-19 14:27:44 -0600

    sandbox-ing with app.js file

--- src/App.js
+++ src/App.js
@@ -8,10 +8,15 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">To Do App</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          <form>
+            "Today's Tasks:" <input type='text' name='Tasks'/>
+          </form>
         </p>
       </div>
     );
@@ -19,3 +24,5 @@ class App extends Component {
 }
 
 export default App;
+// L16-20: this is bad practice, form shouldn't appear as descendant of p but i'm just playing for now
+// L18: also forgot how to use possesive quote and not have it interfere with displaying on site
---
+++ tasks.md
@@ -0,0 +1,6 @@
+1. Basic React App
+  -create-react-app
+  -read googled documentation
+  -put some random crud work in the app
+  -need to go through udemy course
+  
